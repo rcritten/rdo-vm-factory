352f6baf9d69520b74547e4663d0f789d5fdbe76
Rob Crittenden <rcritten@redhat.com>
2015-12-07 11:28:20 -0500

    Ensure file images are removed and cleanup the network as well

--- rdo-ipa-nova/cleanup.sh
+++ rdo-ipa-nova/cleanup.sh
@@ -4,3 +4,10 @@ for vm in ipa openstack ; do
     sudo virsh destroy $vm
     sudo virsh undefine --remove-all-storage $vm
 done
+
+for img in ipa.qcow2 ipa-cidata.iso openstack.qcow2 openstack-cidata.iso; do
+    rm -f /var/lib/libvirt/images/$img
+done
+
+virsh net-destroy oslocaltest
+virsh net-undefine oslocaltest
